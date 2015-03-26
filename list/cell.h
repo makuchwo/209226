26e61b70415802050f7f89e24b65fef118273bc0
Sheaim <209226@pwr.edu.pl>
2015-03-26 08:38:47 +0100

    Lista
    Benchmark działający na liście dwustronnie łączonej.

---
+++ list/cell.h
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <math.h>
+#ifndef  CELLH
+#define  CELLH
+
+class cell{
+
+private:
+/* Deklaracja pól */
+    int value;
+    cell * next;
+    cell * prev;
+
+public:
+
+/* Deklaracja funkcji */
+	void set_value(int value);
+	int get_value();
+    void set_next(cell * next);
+    void set_prev(cell * prev);
+    cell * get_next();
+    cell * get_prev();
+    cell(int value);
+    ~cell();
+
+};
+
+void cell::set_value(int value){
+	this->value=value;
+}
+
+int cell::get_value(){
+	return value;
+}
+
+cell::~cell(){
+}
+
+cell::cell(int value){
+	this->value=value;
+	next=NULL;
+	prev=NULL;
+}
+
+cell * cell::get_next(){
+
+	return this->next;
+}
+
+void cell::set_next(cell * next){
+	this->next=next;
+}
+
+cell * cell::get_prev(){
+	return this->prev;
+}
+
+void cell::set_prev(cell * prev){
+	this->prev=prev;
+}
+
+
+#endif // CELL
