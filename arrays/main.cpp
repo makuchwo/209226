4063b618a4c262a5d3e5b364fdb650747e89eecd
Sheaim <209226@pwr.edu.pl>
2015-03-26 08:39:39 +0100

    Tablice
    Benchmark działający na tablicy. Na razie zaimplementowana tylko wersja
    pojedyncza, z niewyjaśnionych przyczyn wysypuje się przy 100 000+
    elementów.

---
+++ arrays/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include "array.h"
+#include <ctime>
+#include <windows.h>
+#include "spine.h"
+#include "menu.h"
+
+
+
+int main()
+{
+    menu();
+    return 0;
+}
